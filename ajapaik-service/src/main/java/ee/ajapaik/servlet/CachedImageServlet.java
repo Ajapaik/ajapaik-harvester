ceb92e29b044c84c3e52a59cb209ed2fb054ca6f
kaido-kalda <kalda.kaido@gmail.com>
2015-05-15 20:56:03 +0700

    no thumbnail

--- ajapaik-service/src/main/java/ee/ajapaik/servlet/CachedImageServlet.java
+++ ajapaik-service/src/main/java/ee/ajapaik/servlet/CachedImageServlet.java
@@ -7,7 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -20,7 +21,22 @@ import ee.ajapaik.db.Repository;
 public class CachedImageServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private static final Logger logger = Logger.getLogger(CachedImageServlet.class);
+	private byte[] noThumbnail;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		try {
+			byte[] buffer = new byte[2024];
+
+			int length = IOUtils.read(new ClassPathResource("nt.jpg").getInputStream(), buffer);
+			
+			this.noThumbnail = new byte[length];
+			System.arraycopy(buffer, 0, noThumbnail, 0, length);
+		} catch (IOException e) {
+			throw new ServletException(e);
+		}
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String[] requestPath = request.getRequestURI().split("/");
@@ -30,15 +46,9 @@ public class CachedImageServlet extends HttpServlet {
 		Repository repository = ctx.getBean("repository", Repository.class);
 		
 		byte[] data = repository.queryImage(requestFile);
-		if (data != null) {
-			try {
-				response.addHeader("Cache-Control", "max-age=31556926");
-				response.getOutputStream().write(data);
-			} catch (IOException e) {
-				logger.error("Failed to send image data:", e);
-			}
-		}
 		
+		response.addHeader("Cache-Control", "max-age=31556926");
+		response.getOutputStream().write(data != null ? data : noThumbnail);
 		response.getOutputStream().close();
 	}
 }
