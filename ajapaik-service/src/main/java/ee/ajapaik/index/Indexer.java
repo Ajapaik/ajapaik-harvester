289046d0c3e7cdcb91601dfdd10185ba78166985
kristokuiv <kristokuiv@gmail.com>
2016-11-06 19:01:54 +0200

    Delete muis records without image right away, instead of storing them in list and deleting after indexing to avoid java head space error.

--- ajapaik-service/src/main/java/ee/ajapaik/index/Indexer.java
+++ ajapaik-service/src/main/java/ee/ajapaik/index/Indexer.java
@@ -31,6 +31,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static ee.ajapaik.index.IndexedFields.*;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * @author <a href="mailto:kaido@quest.ee?subject=Indexer">Kaido Kalda</a>
@@ -302,6 +303,14 @@ public class Indexer implements InitializingBean {
 			@Override
 			public void handleRecord(Record rec, String code) {
 				if(rec != null) {
+					
+					// FIXME: HACK. Clean up database of wrong muis id format
+					if(code.equals(muisRepoHash) && isBlank(rec.getImageUrl())) {
+						logger.debug("Deleting record without image from Muis Repo. Record id = " + rec.getId() + ", urlToRecord = " + rec.getUrlToRecord());
+						repository.deleteRecord(rec.getId(), muisRepoHash);
+						return;
+					}
+					
 					totalCount.setValue(totalCount.getValue() != null ? totalCount.getValue() + 1 : 1);
 					
 					if(totalCount.getValue() % 1000 == 0) {
