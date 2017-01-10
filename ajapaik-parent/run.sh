c11bf405f71fbe9f5ef86cee161fa4c0e1eb5498
kristokuiv <kristokuiv@gmail.com>
2017-01-10 15:58:52 +0200

    allow 2GB heap space

--- ajapaik-parent/run.sh
+++ ajapaik-parent/run.sh
@@ -1,5 +1,5 @@
 date
 export PATH=$PATH:/home/valimimoodul/apache-maven-2.2.1/bin:/home/valimimoodul/jdk1.6.0_43/bin
-export MAVEN_OPTS=-Xmx512m
+export MAVEN_OPTS=-Xmx2048m
 cd /home/valimimoodul/ajapaik/ajapaik-parent/
 mvn jetty:run
