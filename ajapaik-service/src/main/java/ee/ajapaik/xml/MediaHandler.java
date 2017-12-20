92adcba9e340e37a9a00cb88174f12dda2f9bfa5
kristokuiv <kristokuiv@gmail.com>
2017-12-20 18:35:12 +0200

    MuIS medialist structure is updated

--- ajapaik-service/src/main/java/ee/ajapaik/xml/MediaHandler.java
+++ ajapaik-service/src/main/java/ee/ajapaik/xml/MediaHandler.java
@@ -1,11 +1,11 @@
 package ee.ajapaik.xml;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MediaHandler extends DefaultHandler {
 	
@@ -18,8 +18,10 @@ public class MediaHandler extends DefaultHandler {
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		//rdf:_1 rdf:resource="http://opendata.rahvusarhiiv.tietotest.ee/media/64f9c950a894"/>
-		if(qName.startsWith("rdf:_") || qName.startsWith("rdf:li")) {
+		if(qName.startsWith("rdf:_")) {
 			medias.add(attributes.getValue("rdf:resource"));
+		} else if (qName.startsWith("rdf:Description")) {
+			medias.add(attributes.getValue("rdf:about"));
 		}
 	}
 }
