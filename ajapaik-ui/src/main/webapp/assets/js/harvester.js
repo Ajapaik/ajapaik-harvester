95c8487e95a51528265ed08483cfd32d89772291
kristokuiv <kristokuiv@gmail.com>
2017-12-23 23:36:25 +0200

    Use datepickers to select last harvest date and new schedule

--- ajapaik-ui/src/main/webapp/assets/js/harvester.js
+++ ajapaik-ui/src/main/webapp/assets/js/harvester.js
@@ -1,9 +1,22 @@
 $(function () {
+    $('#schedule').datetimepicker({
+        sideBySide: true,
+        format: 'YYYY-MM-DD HH:mm'
+    })
+    $('#lastHarvestDate').datetimepicker({
+        format: 'YYYY-MM-DD'
+    })
     $('#create-harvester').on('submit', function (e) {
         e.preventDefault()
         createInfoSystem()
     })
 })
+
+function getSchedule() {
+    var dateTime = $('#schedule').val()
+    return '0 ' + dateTime.substr(14, 2) + ' ' + dateTime.substr(11, 2) + ' ' +
+        dateTime.substr(8, 2) + ' ' + dateTime.substr(5, 2) + ' ? ' + dateTime.substr(0, 4)
+}
 
 function createInfoSystem() {
     var params = {}
@@ -11,7 +24,7 @@ function createInfoSystem() {
     params['setsToUse'] = setsToUse ? $.map($('#setsToUse').val().split(","), $.trim).filter(function (set) { return !!set }) : []
     params['name'] = $('#harvesters').val()
     params['lastHarvestTime'] = $('#lastHarvestDate').val()
-    params['schedule'] = $('#schedule').val()
+    params['schedule'] = getSchedule()
     console.log(JSON.stringify(params))
     request('initCustomHarvester', [params], function () {
         $('#harvester-view alert').show()
