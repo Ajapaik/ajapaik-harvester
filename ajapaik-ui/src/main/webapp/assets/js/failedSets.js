afd43d63fd1a158d6ede771ace736796852fe9fa
kristokuiv <kristokuiv@gmail.com>
2017-12-26 21:49:39 +0200

    Sort failed sets by date descending

--- ajapaik-ui/src/main/webapp/assets/js/failedSets.js
+++ ajapaik-ui/src/main/webapp/assets/js/failedSets.js
@@ -1,6 +1,6 @@
 function showFailedSets() {
     request("getFailedSets", {}, function (response) {
-        Object.keys(response).forEach(function (date) {
+        Object.keys(response).sort().reverse().forEach(function (date) {
             $('#failed-sets').append('<h4>'+date+'</h4>').append('<ul></ul>')
             response[date].forEach(function (set) {
                 $('#failed-sets ul').append('<li>'+set+'</li>')
